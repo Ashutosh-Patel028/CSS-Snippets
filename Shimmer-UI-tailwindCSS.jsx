802f03e2bb73211b3d59d69b04b2f8d356e481f1
Ashutosh Patel <57192474+Ashutosh-Patel028@users.noreply.github.com>
2023-06-26 09:50:02 +0530

    updated array method

--- Shimmer-UI-tailwindCSS.jsx
+++ Shimmer-UI-tailwindCSS.jsx
@@ -1,12 +1,10 @@
 //tailwindCSS : v3.3.2
-//customize no. of cards to be shown using this for loop;
-const items=[]
-for(let i=0;i<15;i++) items.push(i);
+//customize no. of cards to be shown by changing the size of new Array();
 
 export default function ShimmerUI(){
     return (
             <div className="flex flex-wrap flex-row w-full justify-between">
-            {items.map((val,ind)=>{
+            {[...new Array(15)].map((val,ind)=>{
                 return (
                 <div key={ind} className="card my-4 mx-7 w-52 h-56 bg-white rounded-lg shadow-2xl p-4 shadow-slate-400">
                     <div className="header w-full h-4/6 rounded-md mb-1.5 bg-slate-200 animate-pulse"></div>
